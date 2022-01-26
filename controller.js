c75cebc021d67aa9b7b8b9c1657b20713be2ab79
yeremiavictor <yeremiavictor@gmail.com>
2022-01-26 10:56:11 +0700

    menambahkan res di controller.js krn error

--- controller.js
+++ controller.js
@@ -5,5 +5,5 @@ var response = require('./res')
 var connection = require('./koneksi')
 
 exports.index = function(req,res){
-    response.ok("Aplikasi Rest Api Berjalan")
+    response.ok("Aplikasi Rest Api Berjalan",res)
 }
