4f10722682279f964c1195e0a1b5cfcda5e8bfd3
yeremiavictor <yeremiavictor@gmail.com>
2022-01-26 13:02:46 +0700

    menambahkan fitur (tambah) di controller & routes

--- controller.js
+++ controller.js
@@ -31,3 +31,20 @@ exports.tampilberdasarkanid = function (req,res) {
         }
     })
 }
+
+//menambahkan data mahasiswa --- harus menggunakan postman
+exports.tambahmahasiswa = function (req,res){
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan
+
+    connection.query('INSERT INTO mahasiswa (nim,nama,jurusan) VALUES(?,?,?)',
+        [nim,nama,jurusan],
+        function(error,rows,fields){
+            if(error){
+                console.log(error)
+            }else{
+                response.ok("Berhasil menambahkan Data", res)
+            }
+        })
+}
