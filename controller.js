37559a86af8d9cd654e1e84f11deae184bd1b559
yeremiavictor <yeremiavictor@gmail.com>
2022-01-26 11:20:15 +0700

    update controller.js res.js install nodemon

--- controller.js
+++ controller.js
@@ -3,7 +3,19 @@
 
 var response = require('./res')
 var connection = require('./koneksi')
+const Connection = require('mysql/lib/Connection')
 
 exports.index = function(req,res){
     response.ok("Aplikasi Rest Api Berjalan",res)
+}
+
+//menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function (req,res){
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fields){
+        if(error){
+            console.log(error)
+        }else{
+            response.ok(rows, res)
+        }
+    })
 }
--- routes.js
+++ routes.js
@@ -5,4 +5,8 @@ module.exports = function(app){
 
     app.route('/')
     .get(jsonku.index)
+
+
+    app.route('/tampil')
+    .get(jsonku.tampilsemuamahasiswa)
 }
