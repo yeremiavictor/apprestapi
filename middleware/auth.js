49d8521febf0758610e6b355a53ca6850062d386
yeremiavictor <yeremiavictor@gmail.com>
2022-01-27 15:20:00 +0700

    menambahkan fitur login

--- middleware/auth.js
+++ middleware/auth.js
@@ -42,3 +42,55 @@ exports.registrasi =  function(req,res){
         }
     })
 }
+
+
+//controller untuk login
+exports.login = function(req,res){
+    var post = {
+        password: req.body.password,
+        email: req.body.email
+    }
+
+    var query = "SELECT * FROM ?? WHERE ??=? AND ??=?"
+    var table=["user", "password", md5(post.password), "email", post.email]
+
+    query = mysql.format(query,table)
+    connection.query(query, function(error,rows){
+        if(error){
+            console.log(error)
+        }else{
+            if(rows.length == 1){
+                var token = jwt.sign({rows}, config.secret,{
+                    expiresIn: 1440 //(25menit)
+                })
+
+                let id_user = rows[0].id
+
+                var data = {
+                    id_user: id_user,
+                    access_token: token,
+                    ip_address: ip.address()
+                }
+
+                var query = "INSERT INTO ?? SET ?"
+                var table = ["akses_token"]
+
+                query = mysql.format(query,table)
+                connection.query(query, data, function(error,rows){
+                    if(error){
+                        console.log(error)
+                    }else{
+                        res.json({
+                            success: true,
+                            message:'Token JWT digenerate',
+                            token:token,
+                            currUser: data.id_user
+                        })
+                    }
+                })
+            } else{
+                res.json({"Error": true, "Message":"Email atau password salah"})
+            }
+        }
+    })
+}
